dbc024639391acddaa6f7bd73429f2d74ee6172d
Polina Leonova <p.leonova@haulmont.com>
2021-08-20 18:52:31 +0400

    lab-3 part 2

--- core/lab-3-core/src/main/java/com/luxoft/springioc/lab3/model/UsualPerson.java
+++ core/lab-3-core/src/main/java/com/luxoft/springioc/lab3/model/UsualPerson.java
@@ -2,6 +2,8 @@ package com.luxoft.springioc.lab3.model;
 
 import java.util.List;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -11,7 +13,7 @@ import org.springframework.stereotype.Service;
 //import javax.annotation.PreDestroy;
 
 @Service("person")
-public class UsualPerson implements Person {
+public class UsualPerson implements Person, InitializingBean, DisposableBean {
 	
 	public static int createdPersons = 0; 
 
@@ -143,4 +145,13 @@ public class UsualPerson implements Person {
         return result;
     }
 
+    @Override
+    public void destroy() throws Exception {
+        createdPersons--;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        createdPersons++;
+    }
 }
