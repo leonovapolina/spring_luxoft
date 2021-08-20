185c7000338bb415fc91eebbea281720f647a195
Polina Leonova <p.leonova@haulmont.com>
2021-08-20 18:46:10 +0400

    lab-3 part 1

--- core/lab-3-core/src/main/java/com/luxoft/springioc/lab3/model/UsualPerson.java
+++ core/lab-3-core/src/main/java/com/luxoft/springioc/lab3/model/UsualPerson.java
@@ -5,29 +5,35 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+//import javax.annotation.PostConstruct;
+//import javax.annotation.PreDestroy;
 
-//@Component("person")
+@Service("person")
 public class UsualPerson implements Person {
 	
 	public static int createdPersons = 0; 
 
-//    @Value("${person.id}")
+    @Value("${person.id}")
     private int id;
 
+    @Value("${person.name}")
     private String name;
 
     @Autowired
     private Country country;
 
+    @Value("${person.age}")
     private int age;
 
+    @Value("${person.height}")
     private float height;
 
+    @Value("${person.isProgrammer}")
     private boolean isProgrammer;
 
+    @Value("${person.isRegistered}")
     private boolean isRegistered;
 
 	private List<String> contacts;
